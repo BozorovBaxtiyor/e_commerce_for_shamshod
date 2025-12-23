da272e10f37365bfa325bffa4ba47638c92cc3be
BaxtiyorBozorov <baxtiyorbozorov226@gmail.com>
2025-12-23 12:06:49 +0500

    Update CORS configuration to allow requests from any origin

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
